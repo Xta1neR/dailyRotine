55e2ae1712d3f53895c94e9a426de31d82e8768c
Rituraj Goswami <83760028+Xta1neR@users.noreply.github.com>
2026-06-27 23:22:09 +0530

    Update routine schedule times and titles

--- components/Routine.js
+++ components/Routine.js
@@ -2,14 +2,13 @@ import { Sun, Dumbbell, Coffee, BookOpen, Utensils, PenTool, Moon } from 'lucide
 import styles from './Routine.module.css';
 
 const routineItems = [
-  { time: '07:00 AM', title: 'Wake Up', icon: Sun },
-  { time: '07:15 AM', title: 'Workout', icon: Dumbbell },
+  { time: '08:00 AM', title: 'Wake Up', icon: Sun },
   { time: '08:30 AM', title: 'Breakfast + Office', icon: Coffee },
-  { time: '06:00 PM', title: 'IAS Classes - StudyIQ', icon: BookOpen },
+  { time: '05:15 AM', title: 'Workout', icon: Dumbbell },
+  { time: '06:30 PM', title: 'IAS Classes - StudyIQ', icon: BookOpen },
   { time: '09:00 PM', title: 'Dinner', icon: Utensils },
-  { time: '10:00 PM', title: 'CSAT Practice', icon: PenTool },
-  { time: '11:00 PM', title: 'IAS Classes - StudyIQ', icon: BookOpen },
-  { time: '01:00 AM', title: 'Sleep', icon: Moon },
+  { time: '10:00 PM', title: 'IAS Classes - StudyIQ', icon: PenTool },
+  { time: '03:00 AM', title: 'Sleep', icon: Moon },
 ];
 
 export default function Routine() {
